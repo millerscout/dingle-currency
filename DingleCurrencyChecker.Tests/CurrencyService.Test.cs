180e70483a01b3a878bdae65aa2b96268cd5dd2f
Antonio Marcos S S Jr <amarcosju@gmail.com>
2018-07-04 23:23:45 -0300

    sample for test coverage.

---
+++ DingleCurrencyChecker.Tests/CurrencyService.Test.cs
@@ -0,0 +1,19 @@
+﻿using DingleCurrencyChecker.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DingleCurrencyChecker.Tests
+{
+    [TestClass]
+    public class CurrencyServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var service = new CurrencyService();
+
+            service.PutValue(2);
+
+            Assert.IsTrue(service.Batat == 2);
+        }
+    }
+}
---
+++ DingleCurrencyCheckerCore/CurrencyService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DingleCurrencyChecker.Core
+{
+    public class CurrencyService
+    {
+        public int Batat { get; set; }
+
+        public void PutValue(int x)
+        {
+            Batat = x;
+        }
+    }
+}
